6fdb1c73083a4bb9e177e21781b355e105ad351c
Leo Constantin <lcon69184@gmail.com>
2026-05-16 10:23:08 +0200

    added card component to dashboard

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 // import "./globals.css"
 
 import "@workspace/ui/globals.css"
+import Providers from "@/components/providers"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
