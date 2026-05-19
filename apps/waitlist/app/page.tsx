aac8c3f5959dcd151d0e95d6c680cc100836fc51
Leo Constantin <lcon69184@gmail.com>
2026-05-19 21:33:02 +0200

    finished waiting list

--- apps/waitlist/app/page.tsx
+++ apps/waitlist/app/page.tsx
@@ -1,10 +1,17 @@
+import { Spotlight } from "@/components/custom/spotlight";
+import { HomeLines } from "@/components/lines";
 import { ModeSwitcher } from "@/components/mode-switcher";
 import { WaitlistSection } from "@/components/waitlist";
 export default function Home() {
   return (
     <>
+      <Spotlight
+        className="-top-40 left-0 md:-top-20 md:left-60"
+        fill="white"
+      />
       <WaitlistSection />
-      <div className="absolute top-4 right-4">
+      <HomeLines />
+      <div className="absolute right-4 bottom-4">
         <ModeSwitcher />
       </div>
     </>
