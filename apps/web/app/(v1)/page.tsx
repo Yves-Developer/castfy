41ce3cb1137eca645a7705dced37f4f32b9f3e4f
Leo Constantin <lcon69184@gmail.com>
2026-05-20 18:43:15 +0200

    updated hero section and header

--- apps/web/app/(v1)/page.tsx
+++ apps/web/app/(v1)/page.tsx
@@ -1,10 +1,12 @@
 import { HomeFeatures } from "@/features/home/features";
 import { HomeHero } from "@/features/home/hero";
 import { HomePricing } from "@/features/home/pricing";
+import HomeThumbnail from "@/features/home/thumbnail";
 export default function Home() {
   return (
-    <div className="mx-auto flex max-w-6xl flex-col @md:gap-30 gap-20">
+    <div className="flex flex-col @md:gap-30 gap-20">
       <HomeHero />
+      <HomeThumbnail />
       <HomeFeatures />
       <HomePricing />
     </div>
--- apps/web/features/_layout/site-header.tsx
+++ apps/web/features/_layout/site-header.tsx
@@ -2,57 +2,46 @@
 import { Button } from "@workspace/ui/components/button";
 import { useScroll } from "@workspace/ui/hooks/use-scroll";
 import { cn } from "@workspace/ui/lib/utils";
-import { Logo } from "@/components/logo";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { navLinks } from "@/config/data";
+import { siteConfig } from "@/config/site";
 import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
   const scrolled = useScroll(10);
+  const pathname = usePathname();
 
   return (
     <header
       className={cn(
-        "sticky top-0 z-50 mx-auto w-full max-w-6xl border-transparent border-b md:rounded-full md:border md:transition-all md:ease-out",
-        {
-          "border-border bg-background/95 backdrop-blur-sm supports-backdrop-filter:bg-background/50 md:top-2 md:max-w-5xl md:shadow-oo":
-            scrolled,
-        }
+        "sticky top-0 z-50 border-transparent border-b bg-background",
+        scrolled && "border-border"
       )}
     >
-      <nav
-        className={cn(
-          "container flex h-14 w-full items-center justify-between md:h-12 md:transition-all md:ease-out",
-          {
-            "md:px-2": scrolled,
-          }
-        )}
-      >
-        <a
-          className="rounded-md p-2 hover:bg-muted dark:hover:bg-muted/50"
-          href="/"
-        >
-          <Logo className="h-4" />
-        </a>
-        <div className="hidden items-center gap-2 md:flex">
-          <div>
-            {navLinks.map((link) => (
-              <Button
-                asChild
-                className="rounded-full"
-                key={link.label}
-                size="sm"
-                variant="ghost"
-              >
-                <a href={link.href}>{link.label}</a>
-              </Button>
-            ))}
-          </div>
-          <Button className="rounded-full" size="sm" variant="outline">
-            Sign In
+      <nav className="container flex h-14 w-full items-center justify-between md:h-12">
+        <Link className="font-plus-jakarta-sans font-semibold text-xl" href="/">
+          {siteConfig.name}
+        </Link>
+        <div className="hidden items-center gap-9 md:flex">
+          {navLinks.map((link) => (
+            <Link
+              className={cn(
+                "font-medium text-muted-foreground text-sm hover:text-foreground",
+                pathname === link.href && "text-foreground"
+              )}
+              href={link.href}
+              key={link.label}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
+        <div className="flex items-center gap-3">
+          <Button size="sm" variant="outline">
+            Log in
           </Button>
-          <Button className="rounded-full" size="sm">
-            Get Started
-          </Button>
+          <Button size="sm">Sign up</Button>
         </div>
         <MobileNav />
       </nav>
