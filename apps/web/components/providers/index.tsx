05ff93fa27aa16e0b0a112b1b1a80752b027fc3b
Leo Constantin <lcon69184@gmail.com>
2026-08-01 20:56:25 +0200

    added analytics on web

--- apps/web/components/providers/index.tsx
+++ apps/web/components/providers/index.tsx
@@ -2,13 +2,16 @@
 
 import { Toaster } from "@castfy/ui/components/sonner";
 import { TailwindIndicator } from "@castfy/ui/components/tailwind-indicator";
+import { PostHogProvider } from "./posthog";
 import { ThemeProvider } from "./theme-provider";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider>
-      {children}
-      <TailwindIndicator />
-      <Toaster />
-    </ThemeProvider>
+    <PostHogProvider>
+      <ThemeProvider>
+        {children}
+        <TailwindIndicator />
+        <Toaster />
+      </ThemeProvider>
+    </PostHogProvider>
   );
 }
